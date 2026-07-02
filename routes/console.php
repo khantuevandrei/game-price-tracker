88bd802bb3a16fb60e6a5eafb517d13e4cb5fa76
Khantuev Andrei <khantuevandrei@gmail.com>
2026-07-02 19:05:13 +0500

    add scheduler and worker services, register prices:fetch hourly

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('prices:fetch')
+    ->hourly()
+    ->appendOutputTo(storage_path('logs/prices-fetch.log'));
