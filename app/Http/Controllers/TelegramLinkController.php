5a5dcedfc99e7b504f29e91b0225023207c34fdf
Khantuev Andrei <khantuevandrei@gmail.com>
2026-07-02 09:29:53 +0500

    style: fix pint issues

--- app/Http/Controllers/TelegramLinkController.php
+++ app/Http/Controllers/TelegramLinkController.php
@@ -11,12 +11,12 @@ class TelegramLinkController extends Controller
     {
         $code = strtoupper(substr(bin2hex(random_bytes(4)), 0, 6));
 
-        Cache::put('tg_link:' . $code, $request->user()->id, 600);
+        Cache::put('tg_link:'.$code, $request->user()->id, 600);
 
         return response()->json([
             'code' => $code,
             'bot_username' => env('TELEGRAM_BOT_USERNAME'),
-            'expires_in' => 600
+            'expires_in' => 600,
         ]);
     }
 
